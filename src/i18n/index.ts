275b58f2f7f89da6a8a43c20a3c726e4278d9515
unknown <mysael.ti@gmail.com>
2023-01-26 16:53:11 -0300

    iniciando com o i18n

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,4 +1,10 @@
 import { i18n } from "i18next";
-
+import { initReactI18next } from "react-i18next";
+import PT from './locales/pt/pt-br.json';
+import EN from './locales/en/en-us.json';
+import ES from './locales/es/es-es.json';
+import IT from './locales/it/it-it.json';
+import FR from './locales/fr/fr-fr.json';
+import DE from './locales/de/de-de.json';
 
 export default i18n;
