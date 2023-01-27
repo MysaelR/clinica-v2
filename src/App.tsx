ee0c4e50430e26e06e1820efd335b668c7273433
unknown <mysael.ti@gmail.com>
2023-01-27 16:58:15 -0300

    tive que parar mas vou continuar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
 
 function App() {
+  const { t } = useTranslation();
   return (
     <div>
-      
+      {t('Learn I18N')}
     </div>
   );
 }
--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,4 +1,4 @@
-import { i18n } from "i18next";
+import i18n  from "i18next";
 import { initReactI18next } from "react-i18next";
 import PT from './locales/pt/pt-br.json';
 import EN from './locales/en/en-us.json';
@@ -6,5 +6,29 @@ import ES from './locales/es/es-es.json';
 import IT from './locales/it/it-it.json';
 import FR from './locales/fr/fr-fr.json';
 import DE from './locales/de/de-de.json';
+
+
+// let translate: i18n;
+
+export const resources = {
+    'pt-br': PT,
+    'en-en': EN,
+    'es-es': ES,
+    'it-it': IT,
+    'fr-fr': FR,
+    'de-de': DE
+}
+
+i18n
+    .use(initReactI18next)
+    .init({
+        resources,
+        lng: navigator.language,
+        interpolation: {
+            
+        }
+    })
+
+
 
 export default i18n;
